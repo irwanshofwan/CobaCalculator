21aa47cf93c2ee5c5694773863e6427cab5bfe55
Irwan Shofwan <irwan.shofwan@go-jek.com>
2020-03-16 13:20:48 +0700

    Add Calculator add method to add number

---
+++ src/main/java/com/bootcamp/math/Calculator.java
@@ -0,0 +1,15 @@
+package com.bootcamp.math;
+
+public class Calculator {
+
+  private double result;
+
+  public void add(int number) {
+    result += number;
+  }
+
+  @Override
+  public String toString() {
+    return String.valueOf(result);
+  }
+}
---
+++ src/test/java/com/bootcamp/math/CalculatorTest.java
@@ -0,0 +1,14 @@
+package com.bootcamp.math;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+  @Test
+  public void testAdd() {
+    Calculator calculator = new Calculator();
+    calculator.add(1);
+    Assert.assertEquals("1.0", calculator.toString());
+  }
+}
